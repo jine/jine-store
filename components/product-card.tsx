43528f7c129260b6f712d33ecdb37994b7b5850a
Jim Nelin <jim@jine.se>
2026-01-28 12:19:22 +0100

    Additional styling on cards

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -3,25 +3,21 @@ import type { Product } from "@/lib/types";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="overflow-hidden relative bg-white">
+    <div className="grid relative p-3 bg-white border rounded-md">
 
-      <div className="overflow-hidden bg-neutral-100 aspect-square">
+      <div className="bg-neutral-100">
         <Image src={product.thumbnail} alt={product.title} width={500} height={500}
           className="object-cover w-full" />
-      </div>
 
-      <div className="flex gap-2 items-start justify-between">
-        <h3 className="font-semibold text-foreground text-sm uppercase">{product.title}</h3>
-        <p className="font-bold text-foreground text-sm">${product.price}</p>
+        <h3 className="absolute top-0 p-3 font-semibold text-foreground text-m bleft-0">{product.title}</h3>
+        <h3 className="absolute top-8 p-3">{product.brand}</h3>
+        <h3 className="absolute top-16 p-3 text-sm uppercase">{product.category}</h3>
+        <p className="absolute right-0 top-0 p-3 font-bold text-foreground text-m">{Math.floor(product.price)*10} SEK</p>
+
       </div>
 
       <p className="mt-1 text-muted">{product.description}</p>
 
-      <div className="mt-auto pt-4">
-        <div className="flex gap-2 items-center">
-            {product.brand} - {product.category}
-        </div>
-      </div>
     </div>
   );
 }
--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -1,9 +1,9 @@
 import type { Product } from "@/lib/types";
-import { ProductCard } from "./product-card";
+import { ProductCard } from "@/components/product-card";
 
 export function ProductGrid({ products }: { products: Product[] }) {
   return (
-    <div className="grid grid-cols-4 gap-6">
+    <div className="grid grid-cols-4 gap-4">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
