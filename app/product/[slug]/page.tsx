3296a4ffff194f1ecd84f6efcaa514e6cd7546e8
Jim Nelin <jim@jine.se>
2026-01-29 09:29:09 +0100

    Missed committing files before.

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import type { Product } from "@/lib/types";
+import { ViewProduct } from "@/components/view-product";
+
+async function getProduct(slug: string): Promise<Product> {
+
+    const productId = slug.split('-')[0];
+
+    const response = await fetch(`https://dummyjson.com/products/${productId}`);
+    const data = await response.json();
+    return data;
+}
+
+export default async function ProductPage({params}: {params: Promise<{ slug: string }>}) {
+  const { slug } = await params
+  const product = await getProduct(slug)
+ 
+  return (
+    ViewProduct({product})
+  )
+}
---
+++ components/view-product.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import type { Product } from "@/lib/types";
+
+export function ViewProduct({ product }: { product: Product }) {
+  return (
+    <div className="container mx-auto px-4grid p-3 bg-white">
+
+      <div className="grid grid-cols-2 mb-4">
+        
+        <h2 className="p-3 font-semibold text-foreground text-m">{product.title}</h2>
+
+        <Image src={product.thumbnail} alt={product.title} width={500} height={500} className="object-cover" />
+        
+
+        <h3 className="p-3">{product.brand}</h3>
+        <h3 className="p-3 text-sm">Kategori: {product.category}</h3>
+        <p className="p-3 font-bold text-foreground text-m">{Math.floor(product.price)*10} SEK</p>
+
+      </div>
+
+      <p className="mt-1">{product.description}</p>
+
+    </div>
+  );
+}
