6703d54aad7e61c9e9e6ecf170179dd534f6564d
Jim Nelin <jim@jine.se>
2026-01-29 09:17:39 +0100

    WIHO; i got a working product page! :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
 import { ProductGrid } from "@/components/product-grid";
 import type { Product, ProductFetch } from "@/lib/types";
+import { Suspense } from "react";
 
 async function fetchProducts(): Promise<ProductFetch> {
 	const response = await fetch('https://dummyjson.com/products?limit=30');
@@ -19,9 +20,11 @@ export default async function Home() {
       <Hero />
 
       <div className="container mx-auto px-4">
-          <h2 className="pb-4 font-bold text-2xl">Products</h2>
+        <h2 className="pb-4 font-bold text-2xl">Products</h2>
 
+        <Suspense fallback={<div>Loading...</div>}>
           <ProductGrid products={jsondata.products} />
+        </Suspense>
       </div>
     </div>
   );
--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import type { Product } from "@/lib/types";
+import Link from "next/link";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="grid relative p-3 bg-white border rounded-md">
 
       <div>
-        <Image src={product.thumbnail} alt={product.title} width={500} height={500} className="object-cover w-full" />
+        <Link href={`/product/${product.id}-${product.title.toLowerCase().replace(/\s+/g, '-')}`}>
+          <Image src={product.thumbnail} alt={product.title} width={500} height={500} className="object-cover w-full" />
+        </Link>
 
         <h3 className="absolute top-0 p-3 font-semibold text-foreground text-m bleft-0">{product.title}</h3>
         <h3 className="absolute top-8 p-3">{product.brand}</h3>
