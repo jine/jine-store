167a6dbe5b7aac4962104c01aff6be081ac74930
Jim Nelin <jim@jine.se>
2026-02-02 11:51:07 +0100

    Minor fix + test witn next.config with URL-format

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -28,7 +28,7 @@ export async function ProductGrid({
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                     {products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
+                        <ProductCard product={product} />
                     ))}
                 </div>
             )}
--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
-    images: {
+    /*images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "cdn.dummyjson.com",
             },
-        ],
+        ],*/
+    images: {
+        remotePatterns: [new URL('https://cdn.dummyjson.com/**')],
     },
 };
 
