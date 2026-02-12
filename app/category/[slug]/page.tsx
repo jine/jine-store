21a93221ea272bec489252f0602ee4d9ab841b4f
Jim Nelin <jim@jine.se>
2026-02-12 21:40:38 +0100

    Moved stuff around a bit more.

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import ViewCategory from "@/components/views/view-category";
+import ViewCategory from "@/components/categories/view-category";
 
 /**
  * Generates metadata for a category page.
